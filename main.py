0e9d3397ec050c8854968fb721e2ea4acc06aa41
AwpData <awpdata@gmail.com>
2021-07-19 12:03:25 -0700

    Program finished, added account transfers and cleaned up code

--- main.py
+++ main.py
@@ -21,9 +21,19 @@ def get_card_num(account, pin_num):
                             (account, pin_num,)).fetchone())
 
 
+def find_card(account):
+    with conn:
+        return cur.execute("SELECT number FROM card WHERE number = ?", (account,)).fetchone()
+
+
 def get_balance(account):
     with conn:
         return cur.execute("SELECT balance FROM card WHERE number = ?", (account,)).fetchone()[0]
+
+
+def remove_income(account, amount):
+    with conn:
+        return cur.execute("UPDATE card SET balance = balance - ? WHERE number = ?", (amount, account,)).fetchone()
 
 
 def add_income(account, amount):
@@ -36,8 +46,24 @@ def delete_card(account):
         cur.execute("DELETE FROM card WHERE number = ?", (account,))
 
 
+def luhn_algorithm(account_num):
+    temp_account_num = ""
+    sums = 0
+    for i in range(0, 15):  # step one, DO NOT HAVE A 16th DIGIT!
+        if i % 2 == 0:  # step two, multiply each even index by 2
+            if int(account_num[i]) * 2 > 9:  # optional step three, subtract 9 from those over 9
+                temp_account_num += str(int(account_num[i]) * 2 - 9)
+            else:  # or continue step two
+                temp_account_num += str(int(account_num[i]) * 2)
+        else:  # step two-b, don't adjust the odd indexes, just add them to our modified account_num
+            temp_account_num += account_num[i]
+        sums += int(temp_account_num[i])  # step four, add the current adjusted num to sum and repeat!
+    temp_account_num += str(
+        10 - (sums % 10)) if sums % 10 != 0 else "0"  # gets the checksum, which is what ever the sum is % 10
+    return temp_account_num[15]  # returns the checksum to our account number as the final digit
+
+
 class CreditCard:
-    database = dict()
 
     def __init__(self):
         self.account_num = None
@@ -45,27 +71,11 @@ class CreditCard:
         self.get_account_num()
         self.get_pin()
 
-    def luhn_algorithm(self):
-        temp_account_num = ""
-        sums = 0
-        for i in range(0, 15):  # step one, DO NOT HAVE A 16th DIGIT!
-            if i % 2 == 0:  # step two, multiply each even index by 2
-                if int(self.account_num[i]) * 2 > 9:  # optional step three, subtract 9 from those over 9
-                    temp_account_num += str(int(self.account_num[i]) * 2 - 9)
-                else:  # or continue step two
-                    temp_account_num += str(int(self.account_num[i]) * 2)
-            else:  # step two-b, don't adjust the odd indexes, just add them to our modified account_num
-                temp_account_num += self.account_num[i]
-            sums += int(temp_account_num[i])  # step four, add the current adjusted num to sum and repeat!
-        temp_account_num += str(
-            10 - (sums % 10)) if sums % 10 != 0 else "0"  # gets the checksum, which is what ever the sum is % 10
-        return temp_account_num[15]  # returns the checksum to our account number as the final digit
-
     def get_account_num(self):
         self.account_num = "400000"  # our IIN from the given bank in the problem (4 = bank though)
         for _ in range(6, 15):  # 16th digit will be checksum from luhn_algo
             self.account_num += str(random.randint(0, 9))
-        self.account_num += self.luhn_algorithm()
+        self.account_num += luhn_algorithm(self.account_num)
 
     def get_pin(self):
         self.pin = ""
@@ -120,10 +130,30 @@ while not terminate:
             add_income(card_number, int(input()))
             print("Income was added!")
         elif choice == 3:
-            pass
+            print("Enter card number")
+            transfer_num = str(input())
+            if len(transfer_num) < 16:
+                print("Invalid card length, try again! (16 characters)")
+            elif luhn_algorithm(transfer_num) != transfer_num[len(transfer_num) - 1]:  # Checks Luhn Algo validity
+                print("Probably you made a mistake in the card number. Please try again!")
+            elif find_card(transfer_num) is None:
+                print("Such a card does not exist.")
+            elif find_card(transfer_num)[0] == card_number:
+                print("You can't transfer money to the same account!")
+            else:
+                print("Enter how much money you want to transfer:")
+                transfer_money = int(input())
+                if transfer_money > int(get_balance(card_number)):
+                    print("Not enough money!")
+                else:
+                    remove_income(card_number, transfer_money)
+                    add_income(transfer_num, transfer_money)
+                    print("Success!")
+
         elif choice == 4:
             print("The account has been closed!")
             delete_card(card_number)
+            logged_in = False
         elif choice == 5:
             logged_in = False
             print("\nYou have successfully logged out!\n")
